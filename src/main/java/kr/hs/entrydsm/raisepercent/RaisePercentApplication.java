29945ccc9331aef1a79831a2c8fce45397160141
leeseojune <sung07288346@gmail.com>
2021-12-28 22:41:37 +0900

    📑 :: Project init

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/RaisePercentApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.raisepercent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaisePercentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaisePercentApplication.class, args);
+    }
+
+}
