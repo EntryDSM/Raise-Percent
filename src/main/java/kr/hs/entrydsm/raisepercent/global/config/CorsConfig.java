7543790dd8c45ee90739c42128ad4de67d8e3e97
leeseojune <sung07288346@gmail.com>
2021-12-29 01:18:57 +0900

    📑 :: Cors mapping

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package kr.hs.entrydsm.raisepercent.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("http://localhost:3000");
+    }
+
+}
