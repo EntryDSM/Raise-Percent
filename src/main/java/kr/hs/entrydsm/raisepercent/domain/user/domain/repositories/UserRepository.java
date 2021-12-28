a5277961ae83697f8854dd3b1516dd42fecaa1c2
leeseojune <sung07288346@gmail.com>
2021-12-29 08:56:19 +0900

    ⚡ :: @Repository 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/domain/repositories/UserRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/domain/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package kr.hs.entrydsm.raisepercent.domain.user.domain.repositories;
 
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 }
