45666f87ab149954b33270cb6556375767103047
leeseojune <sung07288346@gmail.com>
2021-12-30 08:57:35 +0900

    📑 :: Company dao

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/company/domain/repositories/CompanyRepository.java
@@ -0,0 +1,11 @@
+package kr.hs.entrydsm.raisepercent.domain.company.domain.repositories;
+
+import kr.hs.entrydsm.raisepercent.domain.company.domain.Company;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, UUID> {
+}
