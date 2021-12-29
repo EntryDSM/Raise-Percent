352445abce0ffcd9dfc85d69ec792ded41ec7d5a
leeseojune <sung07288346@gmail.com>
2021-12-29 23:19:08 +0900

    📑 :: Student entity

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/domain/Student.java
@@ -0,0 +1,45 @@
+package kr.hs.entrydsm.raisepercent.domain.student.domain;
+
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "tbl_student")
+public class Student {
+
+    @Id
+    @Column(length = 60)
+    private String email;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email")
+    private User user;
+
+    @Column(length = 4, nullable = false)
+    private String number;
+
+    @Column(length = 4, nullable = false)
+    private String year;
+
+    @Column(length = 20)
+    private String position;
+
+    @Builder
+    public Student(User user, String number, String year) {
+        this.user = user;
+        this.number = number;
+        this.year = year;
+    }
+
+    public void updatePosition(String position) {
+        this.position = position;
+    }
+
+}
