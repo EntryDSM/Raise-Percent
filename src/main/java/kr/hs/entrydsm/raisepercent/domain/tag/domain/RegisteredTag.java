74655d3c13f88dac26d97faa05d8a55f2b06f3dc
leeseojune <sung07288346@gmail.com>
2021-12-30 01:38:04 +0900

    📑 :: RegisteredTag entity

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/RegisteredTag.java
@@ -0,0 +1,34 @@
+package kr.hs.entrydsm.raisepercent.domain.tag.domain;
+
+import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
+import kr.hs.entrydsm.raisepercent.global.entity.BaseUUIDEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "tbl_registered_tag")
+public class RegisteredTag extends BaseUUIDEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email")
+    private Student student;
+
+    @Builder
+    public RegisteredTag(Tag tag, Student student) {
+        this.tag = tag;
+        this.student = student;
+    }
+
+}
