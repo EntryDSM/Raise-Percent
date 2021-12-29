e02b60de7c134f0754b2b2fb25f4b695e5a426a4
leeseojune <sung07288346@gmail.com>
2021-12-30 02:05:46 +0900

    🧪 :: Document entity test

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/document/domain/DocumentTest.java
@@ -0,0 +1,50 @@
+package kr.hs.entrydsm.raisepercent.domain.document.domain;
+
+import kr.hs.entrydsm.raisepercent.domain.document.domain.types.Type;
+import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DocumentTest {
+
+    private static final Type type = Type.RESUME;
+
+    private static final Student student = Student.builder()
+            .build();
+
+    private static final Document document = Document.builder()
+            .type(type)
+            .student(student)
+            .build();
+
+    @Test
+    void 문서_객체_생성() {
+        Document document = new Document();
+        assertNull(document.getId());
+        assertNull(document.getType());
+        assertNull(document.getLastModifiedAt());
+        assertNull(document.getStudent());
+    }
+
+    @Test
+    void 학생_가져오기() {
+        assertEquals(student, document.getStudent());
+    }
+
+    @Test
+    void 종류_가져오기() {
+        assertEquals(type, document.getType());
+    }
+
+    @Test
+    void 포트폴리오() {
+        Type type = Type.PORTFOLIO;
+        Document document = Document.builder()
+                .student(student)
+                .type(type)
+                .build();
+        assertEquals(type, document.getType());
+    }
+
+}
