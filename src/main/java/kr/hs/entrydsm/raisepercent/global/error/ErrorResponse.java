f3b9abc6322d3d0db3d1dee9eaf48bfad262fcc2
leeseojune <sung07288346@gmail.com>
2021-12-29 22:34:29 +0900

    📑 :: ErrorResponse dto

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/error/ErrorResponse.java
@@ -0,0 +1,19 @@
+package kr.hs.entrydsm.raisepercent.global.error;
+
+import kr.hs.entrydsm.raisepercent.global.error.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final int status;
+    private final String code;
+    private final String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+}
