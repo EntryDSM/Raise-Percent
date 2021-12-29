e8c67889551553e37486509d417e019bec8ce9c1
leeseojune <sung07288346@gmail.com>
2021-12-30 02:05:36 +0900

    📑 :: Document entity

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/Document.java
@@ -0,0 +1,39 @@
+package kr.hs.entrydsm.raisepercent.domain.document.domain;
+
+import kr.hs.entrydsm.raisepercent.domain.document.domain.types.Type;
+import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
+import kr.hs.entrydsm.raisepercent.global.entity.BaseUUIDEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Entity(name = "tbl_document")
+public class Document extends BaseUUIDEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 9)
+    private Type type;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "email")
+    private Student student;
+
+    @Builder
+    public Document(Type type, Student student) {
+        this.type = type;
+        this.student = student;
+    }
+
+}
