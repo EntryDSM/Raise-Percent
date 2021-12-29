3fb5ebe04fbf3f4c930f7dde419cf016c3c8fbd1
leeseojune <sung07288346@gmail.com>
2021-12-29 22:34:13 +0900

    📑 :: RaiseException test

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/global/error/exception/RaiseExceptionTest.java
@@ -0,0 +1,16 @@
+package kr.hs.entrydsm.raisepercent.global.error.exception;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RaiseExceptionTest {
+
+    @Test
+    void 예외_생성() {
+        ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+        RaiseException raiseException = new RaiseException(errorCode);
+        assertEquals(errorCode, raiseException.getErrorCode());
+    }
+
+}
