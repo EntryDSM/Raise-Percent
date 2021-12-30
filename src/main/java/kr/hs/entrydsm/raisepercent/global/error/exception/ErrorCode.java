0ad850bbbbec93e7532daa685ff53a87286af448
leeseojune <sung07288346@gmail.com>
2021-12-31 01:59:07 +0900

    ⚡ :: Exception enum

--- src/main/java/kr/hs/entrydsm/raisepercent/global/error/exception/ErrorCode.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/error/exception/ErrorCode.java
@@ -9,7 +9,12 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    BAD_REQUEST(400, "GLOBAL-400-1", "Bad request.");
+    BAD_REQUEST(400, "GLOBAL-400-1", "Bad request."),
+
+    USER_NOT_FOUND(404, "GLOBAL-404-1", "User not found."),
+    STUDENT_NOT_FOUND(404, "GLOBAL-404-2", "Student not found."),
+    TEACHER_NOT_FOUND(404, "GLOBAL-404-3", "Teacher not found."),
+    HR_NOT_FOUND(404, "GLOBAL-404-4", "Hr not found.");
 
     private final int status;
     private final String code;
