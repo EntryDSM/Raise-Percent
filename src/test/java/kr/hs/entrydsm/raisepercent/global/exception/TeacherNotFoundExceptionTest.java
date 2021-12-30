9a023b21eb404dbc35adbcc35c1619c3bc4f4a64
leeseojune <sung07288346@gmail.com>
2021-12-31 02:00:24 +0900

    🧪 :: TeacherNotFoundException test

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/global/exception/TeacherNotFoundExceptionTest.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.raisepercent.global.exception;
+
+import kr.hs.entrydsm.raisepercent.global.error.exception.ErrorCode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeacherNotFoundExceptionTest {
+
+    @Test
+    void 선생님_존재하지않음_예외() {
+        assertEquals(ErrorCode.TEACHER_NOT_FOUND, TeacherNotFoundException.EXCEPTION.getErrorCode());
+    }
+
+}
