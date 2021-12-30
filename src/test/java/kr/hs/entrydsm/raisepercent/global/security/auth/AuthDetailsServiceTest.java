4e92dc0c50706c48e1808f43041e2778c0fa4735
leeseojune <sung07288346@gmail.com>
2021-12-31 02:02:18 +0900

    🧪 :: AuthDetailsService test

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/global/security/auth/AuthDetailsServiceTest.java
@@ -0,0 +1,30 @@
+package kr.hs.entrydsm.raisepercent.global.security.auth;
+
+import kr.hs.entrydsm.raisepercent.global.security.auth.facade.AuthFacade;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AuthDetailsServiceTest {
+
+    private AuthFacade authFacade = mock(AuthFacade.class);
+
+    private AuthDetailsService authDetailsService = new AuthDetailsService(authFacade);
+
+    @Test
+    void 유저_인증객체_가져오기() {
+        String email = "test@gmail.com";
+
+        when(authFacade.queryUserRole(email, "teacher"))
+                .thenReturn(Type.ROOT);
+
+        for (GrantedAuthority authority : authDetailsService.loadUserByUsername(email, "teacher")
+                     .getAuthorities()) {
+            assertEquals(Type.ROOT.name(), authority.getAuthority());
+        }
+    }
+
+}
