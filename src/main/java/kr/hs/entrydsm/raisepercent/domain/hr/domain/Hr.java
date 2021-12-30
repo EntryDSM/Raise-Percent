a88a8659fc98bc1cb5f40b2e6a9f6e975a12252d
leeseojune <sung07288346@gmail.com>
2021-12-30 20:13:23 +0900

    📑 :: Hr entity

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/hr/domain/Hr.java
@@ -0,0 +1,35 @@
+package kr.hs.entrydsm.raisepercent.domain.hr.domain;
+
+import kr.hs.entrydsm.raisepercent.domain.company.domain.Company;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "tbl_hr")
+public class Hr {
+
+    @Id
+    @Column(length = 60)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "email")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @Builder
+    public Hr(User user, Company company) {
+        this.user = user;
+        this.company = company;
+    }
+
+}
