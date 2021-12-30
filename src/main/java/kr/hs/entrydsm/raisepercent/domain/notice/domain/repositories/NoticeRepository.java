105d434b9337d33f3e4f74fba4e76785454aac8e
leeseojune <sung07288346@gmail.com>
2021-12-30 13:10:40 +0900

    📑 :: Notice dao

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/domain/repositories/NoticeRepository.java
@@ -0,0 +1,11 @@
+package kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories;
+
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NoticeRepository extends CrudRepository<Notice, UUID> {
+}
