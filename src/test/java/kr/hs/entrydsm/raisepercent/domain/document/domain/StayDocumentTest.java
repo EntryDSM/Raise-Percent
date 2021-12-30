5b49f3f5a195cfcea17b0c4509a5e4826d827ac8
leeseojune <sung07288346@gmail.com>
2021-12-30 19:45:03 +0900

    🧪 :: StayDocument entity test

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/document/domain/StayDocumentTest.java
@@ -0,0 +1,37 @@
+package kr.hs.entrydsm.raisepercent.domain.document.domain;
+
+import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StayDocumentTest {
+
+    private static final DocumentContentId contentId = DocumentContentId.builder()
+            .build();
+
+    private static final String content = "Test content";
+
+    private static final StayDocument stayDocument = StayDocument.builder()
+            .id(contentId)
+            .content(content)
+            .build();
+
+    @Test
+    void 로컬문서_객체_생성() {
+        StayDocument stayDocument = new StayDocument();
+        assertNull(stayDocument.getId());
+        assertNull(stayDocument.getContent());
+    }
+
+    @Test
+    void 로컬문서_아이디_가져오기() {
+        assertEquals(contentId, stayDocument.getId());
+    }
+
+    @Test
+    void 로컬문서_내용_가져오기() {
+        assertEquals(content, stayDocument.getContent());
+    }
+
+}
