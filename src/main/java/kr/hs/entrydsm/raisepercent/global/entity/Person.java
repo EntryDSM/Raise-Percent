060077cdfe157e6c1946f5dd9e34b318dbf67b5b
leeseojune <sung07288346@gmail.com>
2021-12-31 01:57:58 +0900

    📑 :: 공통 객체

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/entity/Person.java
@@ -0,0 +1,7 @@
+package kr.hs.entrydsm.raisepercent.global.entity;
+
+import kr.hs.entrydsm.raisepercent.global.security.auth.Type;
+
+public abstract class Person {
+    public abstract Type queryType();
+}
