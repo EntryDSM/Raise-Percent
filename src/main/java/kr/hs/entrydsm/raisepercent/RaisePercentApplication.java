83bfb50bad5cacaf95c050af60e85fba6fa85775
leeseojune <sung07288346@gmail.com>
2022-01-02 17:25:26 +0900

    ⚡ :: PropertiesScan 활성화

--- src/main/java/kr/hs/entrydsm/raisepercent/RaisePercentApplication.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/RaisePercentApplication.java
@@ -2,9 +2,11 @@ package kr.hs.entrydsm.raisepercent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class RaisePercentApplication {
 
