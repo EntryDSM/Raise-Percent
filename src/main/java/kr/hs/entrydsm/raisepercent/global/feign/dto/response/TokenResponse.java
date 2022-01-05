f891f0276d4fc3fd2091ab3527a0de353dfeb624
김범진 <sdpthf@naver.com>
2022-01-05 22:08:23 +0900

    📑 :: TokenResponse

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/feign/dto/response/TokenResponse.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.raisepercent.global.feign.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class TokenResponse {
+
+    private String accessToken;
+
+}
