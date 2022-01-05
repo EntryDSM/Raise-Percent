108f99a559a061d9ac7b1b15a92b004b28bb6ab5
김범진 <sdpthf@naver.com>
2022-01-06 00:31:58 +0900

    🐛 :: 구글 코드 요청 값 비교

--- src/test/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/request/GoogleCodeRequestTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/request/GoogleCodeRequestTest.java
@@ -2,6 +2,7 @@ package kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.request;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class GoogleCodeRequestTest {
@@ -14,6 +15,8 @@ class GoogleCodeRequestTest {
 
     private static final String redirectUri = "https://www.google.co.kr";
 
+    private static final String grantType = "authorization_code";
+
     private static final GoogleCodeRequest request = GoogleCodeRequest.builder()
             .code(code)
             .clientId(clientId)
@@ -23,11 +26,11 @@ class GoogleCodeRequestTest {
 
     @Test
     void 구글_코드_요청() {
-        assertNull(request.getCode());
-        assertNull(request.getClientId());
-        assertNull(request.getClientSecret());
-        assertNull(request.getRedirectUri());
-        assertNull(request.getGrantType());
+        assertEquals(code, request.getCode());
+        assertEquals(clientId, request.getClientId());
+        assertEquals(clientSecret, request.getClientSecret());
+        assertEquals(redirectUri, request.getRedirectUri());
+        assertEquals(grantType, request.getGrantType());
     }
 
 }
