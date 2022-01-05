b722cbcb24ba61df7ac60001f6b6fbd54dc4d47f
김범진 <sdpthf@naver.com>
2022-01-05 22:07:56 +0900

    📑 :: FeignConfig

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/feign/FeignConfig.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.raisepercent.global.feign;
+
+import feign.codec.ErrorDecoder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@EnableFeignClients(basePackages = "kr.hs.entrydsm.raisepercent.global.feign")
+@Import(FeignClientErrorDecoder.class)
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(value = ErrorDecoder.class)
+    public FeignClientErrorDecoder commonFeignErrorDecoder() {
+        return new FeignClientErrorDecoder();
+    }
+
+}
