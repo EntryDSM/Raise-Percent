b86abd0ec39b4c1caf5df49909e4daa31278f9b7
이준서 <ljchan71@gmail.com>
2022-01-06 02:04:47 +0900

    ♻ :: getCurrentDetails 이동

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/facade/UserFacade.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/facade/UserFacade.java
@@ -2,12 +2,9 @@ package kr.hs.entrydsm.raisepercent.domain.user.facade;
 
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
 import kr.hs.entrydsm.raisepercent.domain.user.domain.repositories.UserRepository;
-import kr.hs.entrydsm.raisepercent.global.exception.CredentialsNotFoundException;
 import kr.hs.entrydsm.raisepercent.global.exception.UserNotFoundException;
 import kr.hs.entrydsm.raisepercent.global.facade.AuthFacade;
-import kr.hs.entrydsm.raisepercent.global.security.auth.AuthDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
@@ -16,6 +13,7 @@ public class UserFacade {
 
     private final AuthFacade authFacade;
     private final UserRepository userRepository;
+    private final AuthFacade authFacade;
 
     public User getCurrentUser() {
         return userRepository.findById(authFacade.getCurrentDetails().getUsername())
