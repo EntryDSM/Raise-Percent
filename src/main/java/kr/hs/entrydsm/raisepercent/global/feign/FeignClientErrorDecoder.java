5e9769b412b3be05d93fa3d19cc7396edc67accd
김범진 <sdpthf@naver.com>
2022-01-05 22:07:36 +0900

    📑 :: FeignClientErrorDecoder

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/feign/FeignClientErrorDecoder.java
@@ -0,0 +1,32 @@
+package kr.hs.entrydsm.raisepercent.global.feign;
+
+import feign.FeignException;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import kr.hs.entrydsm.raisepercent.global.feign.exception.OtherBadRequestException;
+import kr.hs.entrydsm.raisepercent.global.feign.exception.OtherExpiredTokenException;
+import kr.hs.entrydsm.raisepercent.global.feign.exception.OtherForbiddenException;
+import kr.hs.entrydsm.raisepercent.global.feign.exception.OtherUnAuthorizedException;
+
+public class FeignClientErrorDecoder implements ErrorDecoder {
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+        if(response.status() >= 400) {
+            switch (response.status()){
+                case 401:
+                    throw OtherUnAuthorizedException.EXCEPTION;
+                case 403:
+                    throw OtherForbiddenException.EXCEPTION;
+                case 419:
+                    throw OtherExpiredTokenException.EXCEPTION;
+                default:
+                    throw OtherBadRequestException.EXCEPTION;
+            }
+        }
+
+        return FeignException.errorStatus(methodKey, response);
+    }
+
+}
