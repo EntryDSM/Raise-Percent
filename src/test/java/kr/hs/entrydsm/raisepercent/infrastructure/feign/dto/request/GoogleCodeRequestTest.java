0181551332d34fde7ce08407c943fad355fcbcee
김범진 <sdpthf@naver.com>
2022-01-06 01:05:16 +0900

    ♻ :: assertNull import 제거

--- src/test/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/request/GoogleCodeRequestTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/request/GoogleCodeRequestTest.java
@@ -3,7 +3,6 @@ package kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.request;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class GoogleCodeRequestTest {
 
