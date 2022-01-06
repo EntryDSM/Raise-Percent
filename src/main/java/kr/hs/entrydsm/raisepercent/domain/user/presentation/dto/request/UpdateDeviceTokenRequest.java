6a8d929e3efc835f2e19e3ce62f78c9c700fb779
leeseojune53 <sung07288346@gmail.com>
2022-01-07 00:41:20 +0900

    :bug: :: NotBlank로 변경

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/UpdateDeviceTokenRequest.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/UpdateDeviceTokenRequest.java
@@ -3,16 +3,16 @@ package kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @NoArgsConstructor
 public class UpdateDeviceTokenRequest {
 
-    @NotEmpty(message = "type은 비어있으면 안됩니다.")
+    @NotBlank(message = "type은 비어있으면 안됩니다.")
     private String type;
 
-    @NotEmpty(message = "device_token은 비어있으면 안됩니다.")
+    @NotBlank(message = "device_token은 비어있으면 안됩니다.")
     private String deviceToken;
 
 }
