40c51d153ffa07c77ebcb89da0a6b3683130dc0f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-06 10:25:34 +0900

    📑 :: CreateFeedbackRequest

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/feedback/presentation/dto/request/CreateFeedbackRequest.java
@@ -0,0 +1,11 @@
+package kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateFeedbackRequest {
+
+    private String content;
+}
