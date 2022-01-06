1f484868ba357880b51e3e94dddce436310e7b7f
김범진 <sdpthf@naver.com>
2022-01-07 00:45:19 +0900

    📑 :: GoogleAuthService

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/teacher/service/GoogleAuthService.java
@@ -0,0 +1,69 @@
+package kr.hs.entrydsm.raisepercent.domain.teacher.service;
+
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.Teacher;
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.repositories.TeacherRepository;
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.types.Role;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
+import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request.CodeRequest;
+import kr.hs.entrydsm.raisepercent.global.properties.AuthProperties;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.JwtTokenProvider;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.dto.TokenResponse;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.client.GoogleAuth;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.client.GoogleInfo;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.request.GoogleCodeRequest;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response.GoogleInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class GoogleAuthService {
+
+    private final GoogleAuth googleAuth;
+    private final GoogleInfo googleInfo;
+    private final AuthProperties authProperties;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final TeacherRepository teacherRepository;
+
+    @Transactional
+    public TokenResponse execute(CodeRequest request) {
+        String accessToken = googleAuth.googleAuth(
+                GoogleCodeRequest.builder()
+                        .code(request.getCode())
+                        .clientId(authProperties.getClientId())
+                        .clientSecret(authProperties.getClientSecret())
+                        .redirectUri(authProperties.getRedirectUrl())
+                        .build()
+        ).getAccessToken();
+
+        GoogleInfoResponse googleInfoResponse = googleInfo.googleInfo(accessToken);
+
+        String email = googleInfoResponse.getEmail();
+        String name = googleInfoResponse.getName();
+
+        saveTeacher(email, name);
+
+        return new TokenResponse(
+                jwtTokenProvider.generateAccessToken(email, "teacher"),
+                jwtTokenProvider.generateRefreshToken(email, "teacher")
+        );
+    }
+
+    private void saveTeacher(String email, String name) {
+        if(teacherRepository.findById(email).isEmpty()) {
+            teacherRepository.save(
+                    Teacher.builder()
+                            .user(
+                                    User.builder()
+                                            .email(email)
+                                            .name(name)
+                                            .build()
+                            )
+                            .role(Role.DEFAULT)
+                            .build()
+            );
+        }
+    }
+
+}
