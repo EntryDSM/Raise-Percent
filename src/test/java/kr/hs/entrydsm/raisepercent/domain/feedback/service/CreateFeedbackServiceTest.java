6b83e4fc75f01b086102582eba314ec1da506a49
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-06 10:54:59 +0900

    ⚡️ :: service mock -> new

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/feedback/service/CreateFeedbackServiceTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/feedback/service/CreateFeedbackServiceTest.java
@@ -1,7 +1,10 @@
 package kr.hs.entrydsm.raisepercent.domain.feedback.service;
 
+import kr.hs.entrydsm.raisepercent.domain.document.domain.Document;
+import kr.hs.entrydsm.raisepercent.domain.document.facade.DocumentFacade;
 import kr.hs.entrydsm.raisepercent.domain.feedback.domain.repositories.FeedbackRepository;
 import kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.request.CreateFeedbackRequest;
+import kr.hs.entrydsm.raisepercent.domain.teacher.facade.TeacherFacade;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -10,11 +13,15 @@ import static org.mockito.Mockito.*;
 
 public class CreateFeedbackServiceTest {
 
-    private static final CreateFeedbackService service = mock(CreateFeedbackService.class);
+    private static final TeacherFacade teacherFacade = mock(TeacherFacade.class);
+
+    private static final DocumentFacade documentFacade = mock(DocumentFacade.class);
+
+    private static final FeedbackRepository feedbackRepository = mock(FeedbackRepository.class);
 
     private static final CreateFeedbackRequest request = mock(CreateFeedbackRequest.class);
 
-    private static final FeedbackRepository feedbackRepository = mock(FeedbackRepository.class);
+    private static final CreateFeedbackService service = new CreateFeedbackService(teacherFacade, documentFacade, feedbackRepository);
 
     @Test
     void 피드백_작성하기() {
@@ -26,6 +33,6 @@ public class CreateFeedbackServiceTest {
 
         service.execute(id, request);
 
-        verify(feedbackRepository, times(1));
+        verify(feedbackRepository, times(1)).save((any()));
     }
 }
