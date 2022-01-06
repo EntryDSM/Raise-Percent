81c486efe59fa6138b806bbd3d049a083e1a976f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-06 10:55:20 +0900

    ⚡️ :: TeacherFacadeTest 코드 수정

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/teacher/facade/TeacherFacadeTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/teacher/facade/TeacherFacadeTest.java
@@ -3,23 +3,11 @@ package kr.hs.entrydsm.raisepercent.domain.teacher.facade;
 import kr.hs.entrydsm.raisepercent.domain.teacher.domain.Teacher;
 import kr.hs.entrydsm.raisepercent.domain.teacher.domain.types.Role;
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class TeacherFacadeTest {
-
-    private static final Authentication authentication = mock(Authentication.class);
-
-    @BeforeEach
-    void securityContextConfig() {
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-    }
 
     @Test
     void 선생님_정보_가져오기() {
