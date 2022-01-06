ea4a9c00e4f51b072e5bba3a3dd4c8cc71491244
이준서 <ljchan71@gmail.com>
2022-01-07 02:05:16 +0900

    🐛 :: 이거 왜터지는지 모르겠어요

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/user/service/UpdateInformationServiceTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/user/service/UpdateInformationServiceTest.java
@@ -1,21 +1,26 @@
 package kr.hs.entrydsm.raisepercent.domain.user.service;
 
+import java.util.Optional;
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
-import kr.hs.entrydsm.raisepercent.domain.user.facade.UserFacade;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.repositories.UserRepository;
 import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request.UpdateInformationRequest;
+import kr.hs.entrydsm.raisepercent.global.facade.AuthFacade;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class UpdateInformationServiceTest {
 
-    private static final UserFacade userFacade = mock(UserFacade.class);
+    private static final UserRepository userRepository = mock(UserRepository.class);
+
+    private static final AuthFacade authFacade = mock(AuthFacade.class);
 
     private static final UpdateInformationRequest request = mock(UpdateInformationRequest.class);
 
-    private static final UpdateInformationService service = new UpdateInformationService(userFacade);
+    private static final UpdateInformationService service = new UpdateInformationService(userRepository, authFacade);
 
     @Test
     void 유저_정보_갱신() {
@@ -24,8 +29,9 @@ class UpdateInformationServiceTest {
 
         User user = User.builder().build();
 
-        when(userFacade.getCurrentUser())
-                .thenReturn(user);
+        // when
+        when(userRepository.findById(any()))
+            .thenReturn(Optional.ofNullable(user));
         when(request.getContactEmail())
                 .thenReturn(contactEmail);
         when(request.getContactTel())
