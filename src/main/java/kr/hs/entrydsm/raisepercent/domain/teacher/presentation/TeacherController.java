857b20dd578bc251865fc5e0e02a4a1ba98d355d
김범진 <sdpthf@naver.com>
2022-01-07 00:45:55 +0900

    ⚡ :: 선생님 구글 OAuth 로그인 컨트롤러

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/teacher/presentation/TeacherController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/teacher/presentation/TeacherController.java
@@ -1,21 +1,28 @@
 package kr.hs.entrydsm.raisepercent.domain.teacher.presentation;
 
+import kr.hs.entrydsm.raisepercent.domain.teacher.service.GoogleAuthService;
 import kr.hs.entrydsm.raisepercent.domain.teacher.service.QueryGoogleAuthLinkService;
+import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request.CodeRequest;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.dto.TokenResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/teacher")
 @RestController
 public class TeacherController {
 
+    private final GoogleAuthService googleAuthService;
     private final QueryGoogleAuthLinkService queryGoogleAuthLinkService;
 
     @GetMapping("/auth")
     public String queryGoogleAuthLink() {
         return queryGoogleAuthLinkService.execute();
+    }
+
+    @PostMapping("/auth")
+    public TokenResponse googleAuthLogin(@RequestBody CodeRequest codeRequest) {
+        return googleAuthService.execute(codeRequest);
     }
 
 }
