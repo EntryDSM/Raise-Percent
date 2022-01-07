f62b4c6c74bffc3e2bd69f430ae54719851941fe
김범진 <sdpthf@naver.com>
2022-01-07 12:09:37 +0900

    🧪 :: GoogleAuthServiceTest

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/teacher/service/GoogleAuthServiceTest.java
@@ -0,0 +1,82 @@
+package kr.hs.entrydsm.raisepercent.domain.teacher.service;
+
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.repositories.TeacherRepository;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.repositories.UserRepository;
+import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request.CodeRequest;
+import kr.hs.entrydsm.raisepercent.global.properties.AuthProperties;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.JwtTokenProvider;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.dto.TokenResponse;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.client.GoogleAuth;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.client.GoogleInfo;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.request.GoogleCodeRequest;
+import kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response.GoogleInfoResponse;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class GoogleAuthServiceTest {
+
+    private static final String baseUrl = "https://www.google.com";
+
+    private static final String clientId = "asdfass1";
+
+    private static final String clientSecret = "asdf12134as";
+
+    private static final String redirectUrl = "https://localhost:3000/callback";
+
+    private static final GoogleAuth googleAuth = mock(GoogleAuth.class);
+
+    private static final GoogleInfo googleInfo = mock(GoogleInfo.class);
+
+    private static final AuthProperties authProperties = new AuthProperties(baseUrl, clientId, clientSecret, redirectUrl);
+
+    private static final UserRepository userRepository = mock(UserRepository.class);
+
+    private static final JwtTokenProvider jwtTokenProvider = mock(JwtTokenProvider.class);
+
+    private static final TeacherRepository teacherRepository = mock(TeacherRepository.class);
+
+    private static final GoogleAuthService googleAuthService = new GoogleAuthService(
+            googleAuth,
+            googleInfo,
+            authProperties,
+            userRepository,
+            jwtTokenProvider,
+            teacherRepository
+    );
+
+    @Test
+    void 선생님_구글_로그인() {
+        String code = "code";
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+        String googleAccessToken = "googleAccessToken";
+
+        when(googleAuth.googleAuth(any(GoogleCodeRequest.class)))
+                .thenReturn(new kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response.TokenResponse(googleAccessToken));
+
+        when(googleInfo.googleInfo(googleAccessToken))
+                .thenReturn(new GoogleInfoResponse());
+
+        when(jwtTokenProvider.generateAccessToken(any(), any()))
+                .thenReturn(accessToken);
+
+        when(jwtTokenProvider.generateRefreshToken(any(), any()))
+                .thenReturn(refreshToken);
+
+        when(teacherRepository.findById(any()))
+                .thenReturn(Optional.empty());
+
+        TokenResponse response = googleAuthService.execute(new CodeRequest(code));
+
+        assertEquals(response.getAccessToken(), accessToken);
+        assertEquals(response.getRefreshToken(), refreshToken);
+
+        verify(teacherRepository, times(1)).save(any());
+    }
+
+}
