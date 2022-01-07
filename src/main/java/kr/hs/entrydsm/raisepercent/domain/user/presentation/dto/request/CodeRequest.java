57ae1d017ba777e485d6cb4aebc4ea55baf32140
김범진 <sdpthf@naver.com>
2022-01-07 14:46:16 +0900

    ⚰ :: @AllArgsConstructor 제거

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/CodeRequest.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/CodeRequest.java
@@ -1,12 +1,10 @@
 package kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class CodeRequest {
 
     private String code;
--- src/main/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/response/TokenResponse.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/response/TokenResponse.java
@@ -1,12 +1,10 @@
 package kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 public class TokenResponse {
 
     private String accessToken;
