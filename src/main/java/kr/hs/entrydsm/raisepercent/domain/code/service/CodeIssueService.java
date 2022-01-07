6710ebec18e1725c8c6da2fd597f06a84599009d
김범진 <sdpthf@naver.com>
2022-01-07 14:24:18 +0900

    ⚡ :: 중복 예외 처리

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/code/service/CodeIssueService.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/code/service/CodeIssueService.java
@@ -2,6 +2,7 @@ package kr.hs.entrydsm.raisepercent.domain.code.service;
 
 import kr.hs.entrydsm.raisepercent.domain.code.domain.Code;
 import kr.hs.entrydsm.raisepercent.domain.code.domain.repositories.CodeRepository;
+import kr.hs.entrydsm.raisepercent.domain.code.facade.CodeAlreadyExistsException;
 import kr.hs.entrydsm.raisepercent.domain.code.facade.CodeFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,6 +20,10 @@ public class CodeIssueService {
     @Transactional
     public String execute() {
         String code = codeFacade.getCode();
+
+        if(codeRepository.findById(id).isPresent()) {
+            throw CodeAlreadyExistsException.EXCEPTION;
+        }
 
         codeRepository.save(Code.builder()
                 .id(id)
