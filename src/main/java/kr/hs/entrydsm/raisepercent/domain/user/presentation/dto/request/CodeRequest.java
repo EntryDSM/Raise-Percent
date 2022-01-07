fb77f7c12e673efd716255480b702ac1fb603529
김범진 <sdpthf@naver.com>
2022-01-07 11:57:44 +0900

    ⚡ :: @AllArgsConstructor 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/CodeRequest.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/CodeRequest.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CodeRequest {
 
     private String code;
--- src/main/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/response/TokenResponse.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/response/TokenResponse.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TokenResponse {
 
     private String accessToken;
