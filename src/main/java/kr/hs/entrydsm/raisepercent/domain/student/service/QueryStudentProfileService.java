14c051090abbb3fd1336ce95a8c94426ac92f848
Lee SeoJune <sung07288346@gmail.com>
2022-01-07 19:18:48 +0900

    ⚡️ :: StudentFacade 사용

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/student/service/QueryStudentProfileService.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/service/QueryStudentProfileService.java
@@ -1,11 +1,10 @@
 package kr.hs.entrydsm.raisepercent.domain.student.service;
 
 import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
-import kr.hs.entrydsm.raisepercent.domain.student.domain.repositories.StudentRepository;
+import kr.hs.entrydsm.raisepercent.domain.student.facade.StudentFacade;
 import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.response.ProfileResponse;
 import kr.hs.entrydsm.raisepercent.domain.tag.facade.TagFacade;
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
-import kr.hs.entrydsm.raisepercent.global.exception.StudentNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +12,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryStudentProfileService {
 
-    private final StudentRepository studentRepository;
+    private final StudentFacade studentFacade;
     private final TagFacade tagFacade;
 
     public ProfileResponse execute(String email) {
-        Student student = studentRepository.findById(email)
-                .orElseThrow(() -> StudentNotFoundException.EXCEPTION);
+        Student student = studentFacade.getStudent(email);
         User user = student.getUser();
 
         return new ProfileResponse(user.getName(), user.getContactEmail(), user.getContactTel(),
