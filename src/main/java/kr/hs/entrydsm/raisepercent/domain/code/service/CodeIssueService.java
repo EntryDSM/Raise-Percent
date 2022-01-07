44e25a67ca2bee38e1ffedef5d503dc2f4b49038
김범진 <sdpthf@naver.com>
2022-01-07 12:55:34 +0900

    📑 :: CodeIssueService

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/code/service/CodeIssueService.java
@@ -0,0 +1,31 @@
+package kr.hs.entrydsm.raisepercent.domain.code.service;
+
+import kr.hs.entrydsm.raisepercent.domain.code.domain.Code;
+import kr.hs.entrydsm.raisepercent.domain.code.domain.repositories.CodeRepository;
+import kr.hs.entrydsm.raisepercent.domain.code.facade.CodeFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CodeIssueService {
+
+    private final CodeFacade codeFacade;
+    private final CodeRepository codeRepository;
+
+    private static final String id = "TEACHERVERIFICATIONCODE";
+
+    @Transactional
+    public String execute() {
+        String code = codeFacade.getCode();
+
+        codeRepository.save(Code.builder()
+                .id(id)
+                .value(code)
+                .build());
+
+        return code;
+    }
+
+}
