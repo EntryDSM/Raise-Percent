80dca53de262959fe14e5767bc6f552ca4187e50
김범진 <sdpthf@naver.com>
2022-01-08 22:42:46 +0900

    🐛 :: 이런 실수를...

--- src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .antMatchers(HttpMethod.GET, "/teachers/auth").permitAll()
                 .antMatchers(HttpMethod.POST, "/teachers/auth").permitAll()
-                .antMatchers(HttpMethod.POST, "/teachers/code").hasRole("ROOT")
+                .antMatchers(HttpMethod.GET, "/teachers/code").hasRole("ROOT")
                 .antMatchers(HttpMethod.POST, "/feedback/{document-id}").hasRole("ROOT")
                 .antMatchers(HttpMethod.GET, "/students/{student-email}").hasAnyRole("ROOT", "SENIOR", "STUDENT",
                         "TEACHER")
