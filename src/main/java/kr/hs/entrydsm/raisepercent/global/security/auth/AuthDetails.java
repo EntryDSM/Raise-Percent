360ada9030c47a923cd879447c212d79328613f0
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-08 23:33:36 +0900

    ⚡️ :: Getter 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/global/security/auth/AuthDetails.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/security/auth/AuthDetails.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.raisepercent.global.security.auth;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 @RequiredArgsConstructor
 public class AuthDetails implements UserDetails {
 
