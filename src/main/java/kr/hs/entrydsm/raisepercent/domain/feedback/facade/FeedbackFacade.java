bc2ffab4bd4b92f692e778a591e2e68e71e79e72
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-08 23:27:54 +0900

    📑 :: FeedbackFacade

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/feedback/facade/FeedbackFacade.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.raisepercent.domain.feedback.facade;
+
+import kr.hs.entrydsm.raisepercent.domain.feedback.domain.Feedback;
+import kr.hs.entrydsm.raisepercent.domain.feedback.domain.repositories.FeedbackRepository;
+import kr.hs.entrydsm.raisepercent.global.exception.FeedbackNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class FeedbackFacade {
+    
+    private final FeedbackRepository feedbackRepository;
+
+    public Feedback getCurrentFeedback(UUID id) {
+        return feedbackRepository.findById(id)
+                .orElseThrow(() -> FeedbackNotFoundException.EXCEPTION);
+    }
+}
