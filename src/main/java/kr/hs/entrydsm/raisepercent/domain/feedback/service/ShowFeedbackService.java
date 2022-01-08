ddd698a90e347f771d8831a0cc15ba094d46001f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-08 23:30:19 +0900

    :bookmark_tabs: :: showFeedbackService

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/feedback/service/ShowFeedbackService.java
@@ -0,0 +1,35 @@
+package kr.hs.entrydsm.raisepercent.domain.feedback.service;
+
+import kr.hs.entrydsm.raisepercent.domain.feedback.domain.Feedback;
+import kr.hs.entrydsm.raisepercent.domain.feedback.facade.FeedbackFacade;
+import kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.response.ShowFeedbackResponse;
+import kr.hs.entrydsm.raisepercent.domain.student.facade.StudentFacade;
+import kr.hs.entrydsm.raisepercent.global.exception.InvalidRoleException;
+import kr.hs.entrydsm.raisepercent.global.facade.AuthFacade;
+import kr.hs.entrydsm.raisepercent.global.security.auth.Type;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ShowFeedbackService {
+
+    private final FeedbackFacade feedbackFacade;
+    private final AuthFacade authFacade;
+    private final StudentFacade studentFacade;
+
+    public ShowFeedbackResponse execute(UUID id) {
+        Feedback feedback = feedbackFacade.getCurrentFeedback(id);
+
+        if (authFacade.getCurrentDetails().getRole() == Type.STUDENT) {
+            if (!feedback.getDocument().getStudent().equals(studentFacade.getCurrentStudent())) {
+                throw InvalidRoleException.EXCEPTION;
+            }
+        }
+
+        return new ShowFeedbackResponse(feedback.getContent());
+    }
+
+}
