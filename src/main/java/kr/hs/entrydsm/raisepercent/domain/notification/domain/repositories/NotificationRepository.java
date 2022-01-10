17329fbdb081e74fcf8b74be233117aac54a275a
Lee SeoJune <sung07288346@gmail.com>
2022-01-10 20:19:23 +0900

    ⚡️ :: findByReceiver

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notification/domain/repositories/NotificationRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notification/domain/repositories/NotificationRepository.java
@@ -1,11 +1,15 @@
 package kr.hs.entrydsm.raisepercent.domain.notification.domain.repositories;
 
 import kr.hs.entrydsm.raisepercent.domain.notification.domain.Notification;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends CrudRepository<Notification, UUID> {
+    List<Notification> findByReceiver(User receiver, Pageable pageable);
 }
