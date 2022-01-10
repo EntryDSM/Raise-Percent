00ac8e29b9f3da6997ef0ba683ced03461c2d58b
Lee SeoJune <sung07288346@gmail.com>
2022-01-10 13:26:45 +0900

    ⚡️ :: /students/tag

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/student/presentation/StudentController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/presentation/StudentController.java
@@ -1,12 +1,14 @@
 package kr.hs.entrydsm.raisepercent.domain.student.presentation;
 
+import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.request.RegisterTagRequest;
 import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.response.ProfileResponse;
 import kr.hs.entrydsm.raisepercent.domain.student.service.QueryStudentProfileService;
+import kr.hs.entrydsm.raisepercent.domain.student.service.RegisterTagService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/students")
@@ -14,10 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     private final QueryStudentProfileService queryStudentProfileService;
+    private final RegisterTagService registerTagService;
 
     @GetMapping("/{student-email}")
     public ProfileResponse queryStudentProfile(@PathVariable("student-email") String email) {
         return queryStudentProfileService.execute(email);
+    }
+
+    @PostMapping("/tag")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void registerTag(@RequestBody @Valid RegisterTagRequest request) {
+        registerTagService.execute(request);
     }
 
 }
