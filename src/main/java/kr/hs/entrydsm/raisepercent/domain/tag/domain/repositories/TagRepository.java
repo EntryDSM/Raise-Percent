034ce6da665028b257bc22f8d742e75e8eff2332
Lee SeoJune <sung07288346@gmail.com>
2022-01-10 18:08:37 +0900

    🐛 :: id type

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
@@ -3,5 +3,7 @@ package kr.hs.entrydsm.raisepercent.domain.tag.domain.repositories;
 import kr.hs.entrydsm.raisepercent.domain.tag.domain.Tag;
 import org.springframework.data.repository.CrudRepository;
 
-public interface TagRepository extends CrudRepository<Tag, String> {
+import java.util.UUID;
+
+public interface TagRepository extends CrudRepository<Tag, UUID> {
 }
