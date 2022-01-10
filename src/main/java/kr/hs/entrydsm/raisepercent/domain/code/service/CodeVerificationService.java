ea36ba428aa74898b1e96558b4e352049dd65348
김범진 <sdpthf@naver.com>
2022-01-10 20:54:05 +0900

    📑 :: CodeVerificationService

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/code/service/CodeVerificationService.java
@@ -0,0 +1,42 @@
+package kr.hs.entrydsm.raisepercent.domain.code.service;
+
+import kr.hs.entrydsm.raisepercent.domain.code.domain.Code;
+import kr.hs.entrydsm.raisepercent.domain.code.domain.repositories.CodeRepository;
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.Teacher;
+import kr.hs.entrydsm.raisepercent.domain.teacher.domain.types.Role;
+import kr.hs.entrydsm.raisepercent.domain.teacher.facade.TeacherFacade;
+import kr.hs.entrydsm.raisepercent.domain.teacher.presentation.dto.request.VerifyTeacherRequest;
+import kr.hs.entrydsm.raisepercent.global.exception.CodeNotFoundException;
+import kr.hs.entrydsm.raisepercent.global.exception.InvalidRoleException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CodeVerificationService {
+
+    private final TeacherFacade teacherFacade;
+    private final CodeRepository codeRepository;
+
+    @Value("${code.value}")
+    private String id;
+
+    @Transactional
+    public void execute(VerifyTeacherRequest request) {
+        Teacher teacher = teacherFacade.getCurrentTeacher();
+
+        String value = codeRepository.findById(id)
+                .map(Code::getValue)
+                .orElseThrow(() -> CodeNotFoundException.EXCEPTION);
+
+        if (value.equals(request.getCode())) {
+            throw InvalidRoleException.EXCEPTION;
+        }
+
+        teacher.updateRole(Role.TEACHER);
+
+    }
+
+}
