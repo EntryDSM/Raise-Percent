2bae9df0a058e070b5c54d30611f4265bd54a321
김범진 <sdpthf@naver.com>
2022-01-10 20:33:47 +0900

    📑 :: VerifyTeacherRequest

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/teacher/presentation/dto/request/VerifyTeacherRequest.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.raisepercent.domain.teacher.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class VerifyTeacherRequest {
+
+    private String code;
+
+}
