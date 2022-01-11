df5a3e7af4720ce0b0854a368448ce7448f322fe
Lee SeoJune <sung07288346@gmail.com>
2022-01-11 12:06:03 +0900

    ⚡️ :: stream으로 간결화

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notification/domain/facade/NotificationFacade.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notification/domain/facade/NotificationFacade.java
@@ -1,6 +1,5 @@
 package kr.hs.entrydsm.raisepercent.domain.notification.domain.facade;
 
-import kr.hs.entrydsm.raisepercent.domain.notification.domain.Notification;
 import kr.hs.entrydsm.raisepercent.domain.notification.domain.repositories.NotificationRepository;
 import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
 import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.response.NotificationElement;
@@ -8,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -18,22 +17,18 @@ public class NotificationFacade {
     private final NotificationRepository notificationRepository;
 
     public List<NotificationElement> queryNotificationElement(User receiver, Pageable pageable) {
-        List<NotificationElement> result = new ArrayList<>();
-        for (Notification notification : notificationRepository.findByReceiver(receiver, pageable)) {
-            result.add(
-                    NotificationElement.builder()
-                            .notificationId(notification.getId().toString())
-                            .icon(notification.getIcon())
-                            .title(notification.getTitle())
-                            .content(notification.getContent())
-                            .createdAt(notification.getCreatedAt())
-                            .isWatch(notification.isWatch())
-                            .type(notification.getType().toString())
-                            .value(notification.getValue())
-                            .build()
-            );
-        }
-        return result;
+        return notificationRepository.findByReceiver(receiver, pageable).stream().map(
+                notification -> NotificationElement.builder()
+                        .notificationId(notification.getId().toString())
+                        .icon(notification.getIcon())
+                        .title(notification.getTitle())
+                        .content(notification.getContent())
+                        .createdAt(notification.getCreatedAt())
+                        .isWatch(notification.isWatch())
+                        .type(notification.getType().toString())
+                        .value(notification.getValue())
+                        .build()
+        ).collect(Collectors.toList());
     }
 
 }
