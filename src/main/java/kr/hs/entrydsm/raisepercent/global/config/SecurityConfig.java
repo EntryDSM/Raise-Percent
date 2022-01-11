a3c056d8686a5bbe9bac3b1e4b864002719ba886
김범진 <sdpthf@naver.com>
2022-01-11 22:42:17 +0900

    ⚡ :: 공지사항 등록 permission

--- src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/feedback/{feedback-id}").hasAnyRole("ROOT", "STUDENT")
                 .antMatchers(HttpMethod.GET, "/students/{student-email}").hasAnyRole("ROOT", "SENIOR", "STUDENT", "TEACHER")
                 .antMatchers(HttpMethod.PATCH, "/students/position").hasRole("STUDENT")
+                .antMatchers(HttpMethod.POST, "/notices").hasRole("ROOT")
                 .anyRequest().authenticated()
                 .and().apply(new FilterConfig());
     }
