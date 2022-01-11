51c6326ac01ffa4588504b808e3ffb3309ccc0c0
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-11 19:30:18 +0900

    ⚡️ :: UpdateFeedback 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/feedback/presentation/FeedbackController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/feedback/presentation/FeedbackController.java
@@ -1,14 +1,21 @@
 package kr.hs.entrydsm.raisepercent.domain.feedback.presentation;
 
 import kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.request.CreateFeedbackRequest;
+import kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.request.UpdateFeedbackRequest;
 import kr.hs.entrydsm.raisepercent.domain.feedback.presentation.dto.response.ShowFeedbackResponse;
 import kr.hs.entrydsm.raisepercent.domain.feedback.service.CreateFeedbackService;
 import kr.hs.entrydsm.raisepercent.domain.feedback.service.ShowFeedbackService;
+import kr.hs.entrydsm.raisepercent.domain.feedback.service.UpdateFeedbackService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @RequestMapping("/feedback")
@@ -17,15 +24,22 @@ public class FeedbackController {
 
     private final CreateFeedbackService createFeedbackService;
     private final ShowFeedbackService showFeedbackService;
+    private final UpdateFeedbackService updateFeedbackService;
 
     @PostMapping("/{document-id}")
-    public void createFeedback(@PathVariable("document-id") UUID id,
+    public void createFeedback(@PathVariable("document-id") String id,
                                @RequestBody @Valid CreateFeedbackRequest request) {
         createFeedbackService.execute(id, request);
     }
 
     @GetMapping("/{feedback-id}")
-    public ShowFeedbackResponse showFeedback(@PathVariable("feedback-id") UUID id) {
+    public ShowFeedbackResponse showFeedback(@PathVariable("feedback-id") String id) {
         return showFeedbackService.execute(id);
+    }
+
+    @PatchMapping("/{feedback-id}")
+    public ShowFeedbackResponse updateFeedback(@PathVariable("feedback-id") String id,
+                                               @RequestBody @Valid UpdateFeedbackRequest request) {
+        return updateFeedbackService.execute(id, request);
     }
 }
