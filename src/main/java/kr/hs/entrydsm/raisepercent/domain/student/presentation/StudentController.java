ec0a01e74ad7effe2248d755b7a400d6821cb65d
Lee SeoJune <sung07288346@gmail.com>
2022-01-11 12:57:34 +0900

    ⚡️ :: /students/position

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/student/presentation/StudentController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/presentation/StudentController.java
@@ -1,18 +1,22 @@
 package kr.hs.entrydsm.raisepercent.domain.student.presentation;
 
 import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.request.RegisterTagRequest;
+import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.request.UpdatePositionRequest;
 import kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.response.ProfileResponse;
 import kr.hs.entrydsm.raisepercent.domain.student.service.QueryStudentProfileService;
 import kr.hs.entrydsm.raisepercent.domain.student.service.RegisterTagService;
+import kr.hs.entrydsm.raisepercent.domain.student.service.UpdatePositionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ public class StudentController {
 
     private final QueryStudentProfileService queryStudentProfileService;
     private final RegisterTagService registerTagService;
+    private final UpdatePositionService updatePositionService;
 
     @GetMapping("/{student-email}")
     public ProfileResponse queryStudentProfile(@PathVariable("student-email") String email) {
@@ -32,6 +37,12 @@ public class StudentController {
     @ResponseStatus(HttpStatus.CREATED)
     public void registerTag(@RequestBody @Valid RegisterTagRequest request) {
         registerTagService.execute(request);
+    }
+
+    @PatchMapping("/position")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updatePosition(@RequestBody @Valid UpdatePositionRequest request) {
+        updatePositionService.execute(request);
     }
 
 }
