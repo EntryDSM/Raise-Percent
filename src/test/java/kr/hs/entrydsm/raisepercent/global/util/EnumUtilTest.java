9e1c1bb1a82539bea9187d970a9efea23cee0823
Lee SeoJune <sung07288346@gmail.com>
2022-01-11 13:36:49 +0900

    🧪 :: 생성자 테스트

--- src/test/java/kr/hs/entrydsm/raisepercent/global/util/EnumUtilTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/global/util/EnumUtilTest.java
@@ -4,6 +4,10 @@ import kr.hs.entrydsm.raisepercent.domain.student.domain.types.Position;
 import kr.hs.entrydsm.raisepercent.global.exception.InvalidEnumValueException;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EnumUtilTest {
@@ -21,6 +25,15 @@ class EnumUtilTest {
     @Test
     void 잘못된_인수() {
         assertThrows(InvalidEnumValueException.class, () -> EnumUtil.convertToEnum("frontend", Position.class));
+    }
+
+    @Test
+    void 생성() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Constructor<EnumUtil> constructor = EnumUtil.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        InvocationTargetException exception = assertThrows(InvocationTargetException.class, constructor::newInstance);
+        assertEquals("Utility class", exception.getCause().getMessage());
     }
 
 }
