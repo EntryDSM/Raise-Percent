3bdb86b56c23a0721b8c1a56cf24f4ea9683473a
Lee SeoJune <sung07288346@gmail.com>
2022-01-11 15:45:56 +0900

    ⚡️ :: 분야

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/student/domain/types/Position.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/domain/types/Position.java
@@ -1,5 +1,5 @@
 package kr.hs.entrydsm.raisepercent.domain.student.domain.types;
 
 public enum Position {
-    BACKEND, FRONTEND, ANDROID, IOS, DEVOPS, DESIGN, GAME, AI
+    BACKEND, FRONTEND, ANDROID, IOS, DEVOPS, DESIGN, GAME, AI, EMBEDDED, OS
 }
