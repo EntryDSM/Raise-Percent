05ecd98cc15f4717c71fb0e18bf27e45d39cda57
Lee SeoJune <sung07288346@gmail.com>
2022-01-12 13:38:38 +0900

    ⚡️ :: Length

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/UpdateDeviceTokenRequest.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/user/presentation/dto/request/UpdateDeviceTokenRequest.java
@@ -2,6 +2,7 @@ package kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -13,6 +14,7 @@ public class UpdateDeviceTokenRequest {
     private String type;
 
     @NotBlank(message = "device_token은 비어있으면 안됩니다.")
+    @Length(max = 255, message = "device_token은 255자를 넘어서는 안됩니다.")
     private String deviceToken;
 
 }
