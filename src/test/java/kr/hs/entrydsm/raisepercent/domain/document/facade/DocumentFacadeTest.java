e54fe683c3a8348412dd473e8c85dbc184ef50a4
김범진 <sdpthf@naver.com>
2022-01-13 12:37:54 +0900

    🧪 :: 테스트 로직 수정

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/document/facade/DocumentFacadeTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/document/facade/DocumentFacadeTest.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.raisepercent.domain.document.facade;
 
 import kr.hs.entrydsm.raisepercent.domain.document.domain.Document;
+import kr.hs.entrydsm.raisepercent.domain.document.domain.DocumentUserConstant;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.SubmittedDocument;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.repositories.DocumentRepository;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.repositories.SubmittedDocumentRepository;
@@ -15,9 +16,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -33,6 +31,8 @@ class DocumentFacadeTest {
     private static final SubmittedDocumentRepository submittedDocumentRepository = mock(SubmittedDocumentRepository.class);
 
     private static final DocumentFacade documentFacade = new DocumentFacade(documentRepository, submittedDocumentRepository);
+
+    private static final SubmittedDocument submittedDocument = mock(SubmittedDocument.class);
 
     @Test
     void 문서_정보_가져오기() {
@@ -57,20 +57,29 @@ class DocumentFacadeTest {
     @Test
     void 제출한_문서_리스트_가져오기() {
         List<SubmittedDocument> documents = new ArrayList<>();
-        String name = "홍길동";
-        String number = "1234";
+
+        String name = DocumentUserConstant.name;
+        String number = DocumentUserConstant.number;
+        Document document = DocumentUserConstant.document;
+
+        documents.add(submittedDocument);
 
         when(submittedDocumentRepository.findAll())
                 .thenReturn(documents);
 
+        when(submittedDocument.getDocument())
+                .thenReturn(document);
+
+        when(submittedDocument.getId())
+                .thenReturn(id);
+
         List<SubmittedDocumentElement> retDocumentElements = documentFacade.querySubmittedDocumentList();
 
-        retDocumentElements.forEach(document -> {
-            assertThat(document.getName()).isEqualTo(name);
-            assertThat(document.getNumber()).isEqualTo(number);
-            assertThat(document.getSubmittedDocumentId()).isEqualTo(id.toString());
+        retDocumentElements.forEach(element -> {
+            assertThat(element.getName()).isEqualTo(name);
+            assertThat(element.getNumber()).isEqualTo(number);
+            assertThat(element.getSubmittedDocumentId()).isEqualTo(id.toString());
         });
-
     }
 
 }
