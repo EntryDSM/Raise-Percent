ee33f01cd24c0b9b0a48b1ea61dca9e99ff440bb
김범진 <sdpthf@naver.com>
2022-01-13 09:58:26 +0900

    🧪 :: 제출한 문서 리스트 가져오기

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/document/facade/DocumentFacadeTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/document/facade/DocumentFacadeTest.java
@@ -1,15 +1,23 @@
 package kr.hs.entrydsm.raisepercent.domain.document.facade;
 
 import kr.hs.entrydsm.raisepercent.domain.document.domain.Document;
+import kr.hs.entrydsm.raisepercent.domain.document.domain.SubmittedDocument;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.repositories.DocumentRepository;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.repositories.SubmittedDocumentRepository;
+import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.response.SubmittedDocumentElement;
 import kr.hs.entrydsm.raisepercent.global.exception.DocumentNotFoundException;
 import kr.hs.entrydsm.raisepercent.global.util.UUIDUtil;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -44,6 +52,25 @@ class DocumentFacadeTest {
                 .thenReturn(Optional.empty());
 
         assertThrows(DocumentNotFoundException.class, () -> documentFacade.getDocument(any()));
+    }
+
+    @Test
+    void 제출한_문서_리스트_가져오기() {
+        List<SubmittedDocument> documents = new ArrayList<>();
+        String name = "홍길동";
+        String number = "1234";
+
+        when(submittedDocumentRepository.findAll())
+                .thenReturn(documents);
+
+        List<SubmittedDocumentElement> retDocumentElements = documentFacade.querySubmittedDocumentList();
+
+        retDocumentElements.forEach(document -> {
+            assertThat(document.getName()).isEqualTo(name);
+            assertThat(document.getNumber()).isEqualTo(number);
+            assertThat(document.getSubmittedDocumentId()).isEqualTo(id.toString());
+        });
+
     }
 
 }
