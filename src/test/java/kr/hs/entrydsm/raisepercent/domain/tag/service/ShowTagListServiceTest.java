00cf5d7e57d06cead499e3e705a0f4628ed739a0
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-18 10:09:40 +0900

    :bookmark_tabs: :: ShowTagListServiceTest

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/tag/service/ShowTagListServiceTest.java
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.raisepercent.domain.tag.service;
+
+import kr.hs.entrydsm.raisepercent.domain.tag.domain.repositories.TagRepository;
+import kr.hs.entrydsm.raisepercent.domain.tag.presentation.dto.response.ShowTagListResponse;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ShowTagListServiceTest {
+
+    private static final TagRepository tagRepository = mock(TagRepository.class);
+
+    private static final ShowTagListService service = new ShowTagListService(tagRepository);
+    @Test
+    void 태그_리스트_불러오기() {
+        when(tagRepository.findAll())
+                .thenReturn(new ArrayList<>());
+
+        service.execute();
+
+        verify(tagRepository, times(1)).findAll();
+    }
+}
