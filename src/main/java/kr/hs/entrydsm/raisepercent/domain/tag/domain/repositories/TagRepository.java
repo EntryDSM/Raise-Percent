831e87b233228acca875953cca752f02628c097f
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-18 15:18:15 +0900

    :recycle: :: jpa -> crud로 변경 & List<Tag> findAllBy()로 수정

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.raisepercent.domain.tag.domain.repositories;
 
 import kr.hs.entrydsm.raisepercent.domain.tag.domain.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
-public interface TagRepository extends JpaRepository<Tag, UUID> {
+public interface TagRepository extends CrudRepository<Tag, UUID> {
+    List<Tag> findAllBy();
 }
