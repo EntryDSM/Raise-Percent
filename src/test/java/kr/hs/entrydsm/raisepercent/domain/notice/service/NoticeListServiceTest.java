4e0b2975be7c9a63d021fc4d656b0818deb9397d
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-21 21:16:07 +0900

    :bookmark_tabs: :: NoticeListServiceTest

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/notice/service/NoticeListServiceTest.java
@@ -0,0 +1,36 @@
+package kr.hs.entrydsm.raisepercent.domain.notice.service;
+
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories.NoticeRepository;
+import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.response.NoticeListResponse;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class NoticeListServiceTest {
+
+    private static final NoticeRepository noticeRepository = mock(NoticeRepository.class);
+
+    private static final NoticeListService service = new NoticeListService(noticeRepository);
+
+    @Test
+    void 공지사항_목록() {
+        List<Notice> arrayList = new ArrayList<>();
+
+        when(noticeRepository.findAllBy())
+                .thenReturn(arrayList);
+
+        NoticeListResponse response = service.execute();
+
+        assertThat(arrayList).isEqualTo(response.getNoticeList());
+
+        verify(noticeRepository, times(1)).findAllBy();
+    }
+}
