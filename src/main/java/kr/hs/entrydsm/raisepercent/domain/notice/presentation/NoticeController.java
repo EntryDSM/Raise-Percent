8692611371177955450844fd19ce75df05399b72
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2022-01-21 21:14:21 +0900

    ⚡️ :: NoticeList api 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/presentation/NoticeController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/presentation/NoticeController.java
@@ -1,9 +1,12 @@
 package kr.hs.entrydsm.raisepercent.domain.notice.presentation;
 
 import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.request.RegistrationNoticeRequest;
+import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.response.NoticeListResponse;
+import kr.hs.entrydsm.raisepercent.domain.notice.service.NoticeListService;
 import kr.hs.entrydsm.raisepercent.domain.notice.service.RegistrationNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,17 @@ import javax.validation.Valid;
 public class NoticeController {
 
     private final RegistrationNoticeService registrationNoticeService;
+    private final NoticeListService noticeListService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void registerNotice(@RequestBody @Valid RegistrationNoticeRequest request) {
         registrationNoticeService.execute(request);
+    }
+
+    @GetMapping
+    public NoticeListResponse noticeList() {
+        return noticeListService.execute();
     }
 
 }
