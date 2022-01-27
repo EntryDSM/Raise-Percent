268e4aeadbd445789e0f9b3978feb097bb776dfc
김범진 <sdpthf@naver.com>
2022-01-27 23:04:07 +0900

    ⚡ :: JpaRepository -> CrudRepository

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/StayDocumentRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/StayDocumentRepository.java
@@ -2,13 +2,13 @@ package kr.hs.entrydsm.raisepercent.domain.document.domain.repositories;
 
 import kr.hs.entrydsm.raisepercent.domain.document.domain.StayDocument;
 import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface StayDocumentRepository extends JpaRepository<StayDocument, DocumentContentId> {
+public interface StayDocumentRepository extends CrudRepository<StayDocument, DocumentContentId> {
     List<StayDocument> findByIdDocumentId(UUID uuid);
 }
