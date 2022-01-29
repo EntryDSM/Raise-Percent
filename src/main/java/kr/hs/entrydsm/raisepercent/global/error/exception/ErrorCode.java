87feeb446207c78c4e3353d9d7bf2e9be499e25e
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-01-29 14:07:17 +0900

    :recycle: :: ErrorCode 수정

--- src/main/java/kr/hs/entrydsm/raisepercent/global/error/exception/ErrorCode.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/error/exception/ErrorCode.java
@@ -23,8 +23,8 @@ public enum ErrorCode {
     TEACHER_NOT_FOUND(404, "GLOBAL-404-3", "Teacher not found."),
     HR_NOT_FOUND(404, "GLOBAL-404-4", "Hr not found."),
     DOCUMENT_NOT_FOUND(404, "GLOBAL-404-5", "Document not found."),
-    FEEDBACK_NOT_FOUND(404, "GLOBAL-404-6", "Feedback not found."),
-    NOTICE_NOT_FOUND(404, "GLOBAL-404-7", "Notice not found"),
+    FEEDBACK_NOT_FOUND(404, "FEEDBACK-404-1", "Feedback not found."),
+    NOTICE_NOT_FOUND(404, "NOTICE-404-1", "Notice not found"),
   
     CODE_NOT_MATCH(401, "TEACHER-401-1", "Code not match"),
     CODE_NOT_FOUND(404, "TEACHER-404-1", "Code not found"),
