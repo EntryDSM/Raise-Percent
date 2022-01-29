2ea506b59bdc987d910e5b1d49d0c274d181096b
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-01-29 13:17:55 +0900

    :recycle: :: Scope에 따라 조회가능 여부 로직 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/service/ShowNoticeDetailsService.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/service/ShowNoticeDetailsService.java
@@ -1,8 +1,14 @@
 package kr.hs.entrydsm.raisepercent.domain.notice.service;
 
+import kr.hs.entrydsm.raisepercent.domain.hr.domain.repositories.HrRepository;
 import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
 import kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories.NoticeRepository;
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.types.Scope;
 import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.response.NoticeDetailsResponse;
+import kr.hs.entrydsm.raisepercent.domain.student.domain.repositories.StudentRepository;
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
+import kr.hs.entrydsm.raisepercent.domain.user.facade.UserFacade;
+import kr.hs.entrydsm.raisepercent.global.exception.InvalidRoleException;
 import kr.hs.entrydsm.raisepercent.global.exception.NoticeNotFoundException;
 import kr.hs.entrydsm.raisepercent.global.util.UUIDUtil;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +20,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class ShowNoticeDetailsService {
 
     private final NoticeRepository noticeRepository;
+    private final UserFacade userFacade;
+    private final HrRepository hrRepository;
+    private final StudentRepository studentRepository;
 
     @Transactional(readOnly = true)
     public NoticeDetailsResponse execute(String noticeId) {
         Notice notice = noticeRepository.findById(UUIDUtil.convertToUUID(noticeId))
                 .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+
+        User user = userFacade.getCurrentUser();
+
+        if (Scope.COMPANY.equals(notice.getScope()) && hrRepository.findById(user.getEmail()).isPresent()) {
+            throw InvalidRoleException.EXCEPTION;
+        } else if (Scope.STUDENT.equals(notice.getScope()) && studentRepository.findById(user.getEmail()).isPresent()) {
+            throw InvalidRoleException.EXCEPTION;
+        }
 
         return NoticeDetailsResponse.builder()
                 .title(notice.getTitle())
