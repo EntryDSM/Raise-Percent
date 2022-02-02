0f452d56ef48d11416bb0369f635dd4fc3afc795
이준서 <ljchan71@gmail.com>
2022-02-02 23:53:56 +0900

    ⚡ :: updateDocument api

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/presentation/DocumentController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/presentation/DocumentController.java
@@ -1,15 +1,19 @@
 package kr.hs.entrydsm.raisepercent.domain.document.presentation;
 
+import java.util.UUID;
 import javax.validation.Valid;
 import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.request.CreateDocumentRequest;
+import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.request.UpdateDocumentRequest;
 import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.response.CreateDocumentResponse;
 import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.response.SubmittedDocumentListResponse;
 import kr.hs.entrydsm.raisepercent.domain.document.service.ApproveStayDocumentService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.CreateDocumentService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.QuerySubmittedDocumentListService;
+import kr.hs.entrydsm.raisepercent.domain.document.service.UpdateDocumentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DocumentController {
 
 	private final CreateDocumentService createDocumentService;
+	private final UpdateDocumentService updateDocumentService;
 	private final QuerySubmittedDocumentListService querySubmittedDocumentListService;
 	private final ApproveStayDocumentService approveStayDocumentService;
 
@@ -35,6 +40,12 @@ public class DocumentController {
 	@GetMapping("/submit")
 	public SubmittedDocumentListResponse querySubmittedDocumentList() {
 		return querySubmittedDocumentListService.execute();
+	}
+
+	@PatchMapping("/local/{local-document-id}")
+	public void updateDocument(@PathVariable("local-document-id") UUID localId,
+		@RequestBody UpdateDocumentRequest updateDocumentRequest) {
+		updateDocumentService.execute(localId, updateDocumentRequest);
 	}
 
 	@ResponseStatus(HttpStatus.CREATED)
