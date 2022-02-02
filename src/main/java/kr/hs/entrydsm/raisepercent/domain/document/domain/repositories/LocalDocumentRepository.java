2a15d8b0ddc9379fbed027611f9f987b1c9a7b2a
이준서 <ljchan71@gmail.com>
2022-02-02 23:53:42 +0900

    ⚡ :: LocalDocumentRepository

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
@@ -1,10 +1,15 @@
 package kr.hs.entrydsm.raisepercent.domain.document.domain.repositories;
 
+import java.util.UUID;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.LocalDocument;
 import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalDocumentRepository extends CrudRepository<LocalDocument, DocumentContentId> {
+	@Query("delete from LocalDocument l where l.id.document.id = :id")
+	void deleteByDocumentIdByQuery(@Param("id") UUID uuid);
 }
