460050e6bce08228cd1ceb1a30a872ddf1a56988
이준서 <ljchan71@gmail.com>
2022-02-04 01:27:46 +0900

    ⚡ :: LocalDocument findByIdDocumentId

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
@@ -1,5 +1,7 @@
 package kr.hs.entrydsm.raisepercent.domain.document.domain.repositories;
 
+import java.util.List;
+import java.util.UUID;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.LocalDocument;
 import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
 import org.springframework.data.repository.CrudRepository;
@@ -7,4 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocalDocumentRepository extends CrudRepository<LocalDocument, DocumentContentId> {
+	List<LocalDocument> findByIdDocumentId(UUID id_document_id);
 }
