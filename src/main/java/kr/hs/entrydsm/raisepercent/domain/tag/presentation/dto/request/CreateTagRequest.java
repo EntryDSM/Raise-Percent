1ef254849f0f7da605061335c147f94c8da43974
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-04 20:53:38 +0900

    :bookmark_tabs: :: CreateTagRequest

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/presentation/dto/request/CreateTagRequest.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/presentation/dto/request/CreateTagRequest.java
@@ -3,7 +3,13 @@ package kr.hs.entrydsm.raisepercent.domain.tag.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @NoArgsConstructor
-public class CreateTagResponse {
+public class CreateTagRequest {
+
+    @NotBlank(message = "name은 Null, 공백, 띄어쓰기가 허용되지 않습니다.")
+    private String name;
+
 }
