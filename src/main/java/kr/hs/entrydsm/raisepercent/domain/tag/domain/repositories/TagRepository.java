3583a3632dd4711fe5c42709b6c308a86be20fb3
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-04 20:54:23 +0900

    ⚡️ :: Optional<Tag> findByName(String name) 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/domain/repositories/TagRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TagRepository extends CrudRepository<Tag, UUID> {
     List<Tag> findAllByNameContaining(String name);
+    Optional<Tag> findByName(String name);
 }
