668cb4abbb626d209ba0c599c021a4680716fad7
JeongDaeHyun <eogus_2513@dsm.hs.kr>
2022-02-04 20:53:24 +0900

    :bookmark_tabs: :: CreateTagServic

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/service/CreateTagService.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/tag/service/CreateTagService.java
@@ -1,2 +1,25 @@
-package kr.hs.entrydsm.raisepercent.domain.tag.service;public class CreateTagService {
+package kr.hs.entrydsm.raisepercent.domain.tag.service;
+
+import kr.hs.entrydsm.raisepercent.domain.tag.domain.Tag;
+import kr.hs.entrydsm.raisepercent.domain.tag.domain.repositories.TagRepository;
+import kr.hs.entrydsm.raisepercent.domain.tag.exception.AlreadyRegisteredTagException;
+import kr.hs.entrydsm.raisepercent.domain.tag.presentation.dto.request.CreateTagRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CreateTagService {
+
+    private final TagRepository tagRepository;
+
+    public void execute(CreateTagRequest request) {
+        if (tagRepository.findByName(request.getName()).isPresent()) {
+            throw AlreadyRegisteredTagException.EXCEPTION;
+        }
+
+        tagRepository.save(Tag.builder()
+                .name(request.getName())
+                .build());
+    }
 }
