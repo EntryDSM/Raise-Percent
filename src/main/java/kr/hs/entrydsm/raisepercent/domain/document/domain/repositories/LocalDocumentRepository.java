886add76908cf67b63a4a2bf91c8007827681b31
이준서 <ljchan71@gmail.com>
2022-02-06 23:44:07 +0900

    ⚡️ :: 불필요한 import 제거

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/LocalDocumentRepository.java
@@ -3,9 +3,7 @@ package kr.hs.entrydsm.raisepercent.domain.document.domain.repositories;
 import java.util.UUID;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.LocalDocument;
 import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
