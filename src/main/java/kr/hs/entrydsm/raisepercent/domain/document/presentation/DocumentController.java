d18f9a0737c458cdebc7367c2da184e80648a32b
이준서 <ljchan71@gmail.com>
2022-02-08 11:28:20 +0900

    ⚡️ :: deletePublicDocument api

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/presentation/DocumentController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/presentation/DocumentController.java
@@ -8,11 +8,13 @@ import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.response.Cre
 import kr.hs.entrydsm.raisepercent.domain.document.presentation.dto.response.SubmittedDocumentListResponse;
 import kr.hs.entrydsm.raisepercent.domain.document.service.ApproveStayDocumentService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.CreateDocumentService;
+import kr.hs.entrydsm.raisepercent.domain.document.service.DeletePublicDocumentService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.QuerySubmittedDocumentListService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.SubmitDocumentService;
 import kr.hs.entrydsm.raisepercent.domain.document.service.UpdateDocumentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +34,7 @@ public class DocumentController {
 	private final QuerySubmittedDocumentListService querySubmittedDocumentListService;
 	private final ApproveStayDocumentService approveStayDocumentService;
 	private final SubmitDocumentService submitDocumentService;
+	private final DeletePublicDocumentService deletePublicDocumentService;
 
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping
@@ -61,6 +64,12 @@ public class DocumentController {
 	@PostMapping("/local/{local-document-id}")
 	public void submitDocument(@PathVariable("local-document-id") String documentId) {
 		submitDocumentService.execute(documentId);
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/public/{public-document-id}")
+	public void deletePublicDocument(@PathVariable("public-document-id") String documentId) {
+		deletePublicDocumentService.execute(documentId);
 	}
 
 }
