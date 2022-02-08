b577be15386c098781919d36f324b700ed1f3eed
이준서 <ljchan71@gmail.com>
2022-02-08 11:28:33 +0900

    ⚡️ :: PublicDocumentRepository

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/PublicDocumentRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/document/domain/repositories/PublicDocumentRepository.java
@@ -1,5 +1,6 @@
 package kr.hs.entrydsm.raisepercent.domain.document.domain.repositories;
 
+import java.util.UUID;
 import kr.hs.entrydsm.raisepercent.domain.document.domain.PublicDocument;
 import kr.hs.entrydsm.raisepercent.global.entity.DocumentContentId;
 import org.springframework.data.repository.CrudRepository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublicDocumentRepository extends CrudRepository<PublicDocument, DocumentContentId> {
+	void deleteByIdDocumentId(UUID uuid);
 }
