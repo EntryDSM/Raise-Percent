e89bb178b644bae09cee7f6826f430c0f619aa54
cogofoward <hhgoon@naver.com>
2022-02-15 02:06:49 +0900

    📑  :: CompnayNotFoundException

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/exception/CompanyNotFoundException.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.raisepercent.global.exception;
+
+import kr.hs.entrydsm.raisepercent.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.raisepercent.global.error.exception.RaiseException;
+
+public class CompanyNotFoundException extends RaiseException {
+
+    public static final RaiseException EXCEPTION =
+            new CompanyNotFoundException();
+
+    private CompanyNotFoundException() {
+        super(ErrorCode.COMPANY_NOT_FOUND);
+    }
+}
