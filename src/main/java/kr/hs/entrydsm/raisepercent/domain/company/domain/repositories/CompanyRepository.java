a6232873df0e3475418491ab43dff26df44a6c00
cogofoward <hhgoon@naver.com>
2022-02-17 16:45:09 +0900

    📑 :: Create CompanyFacade

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/company/domain/repositories/CompanyRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/company/domain/repositories/CompanyRepository.java
@@ -4,8 +4,12 @@ import kr.hs.entrydsm.raisepercent.domain.company.domain.Company;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, UUID> {
+
+     Optional<Company> findCompanyByName(String name);
+
 }
---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/company/facade/CompanyFacade.java
@@ -0,0 +1,23 @@
+package kr.hs.entrydsm.raisepercent.domain.company.facade;
+
+import kr.hs.entrydsm.raisepercent.domain.company.domain.Company;
+import kr.hs.entrydsm.raisepercent.domain.company.domain.repositories.CompanyRepository;
+import kr.hs.entrydsm.raisepercent.global.exception.CompanyNotFoundException;
+import kr.hs.entrydsm.raisepercent.global.util.UUIDUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class CompanyFacade {
+
+    private final CompanyRepository companyRepository;
+
+    public Company getCompany(String companyName){
+        return companyRepository.findCompanyByName(companyName)
+                .orElseThrow(() -> CompanyNotFoundException.EXCEPTION);
+    }
+
+}
