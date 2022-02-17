d5e0dadeda97936148d68c92d9e3bcb1b1c96e25
cogofoward <hhgoon@naver.com>
2022-02-17 16:45:28 +0900

    🧪 :: CompanyFacadeTest

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/company/facade/CompanyFacadeTest.java
@@ -0,0 +1,46 @@
+package kr.hs.entrydsm.raisepercent.domain.company.facade;
+
+import kr.hs.entrydsm.raisepercent.domain.company.domain.Company;
+import kr.hs.entrydsm.raisepercent.domain.company.domain.repositories.CompanyRepository;
+import kr.hs.entrydsm.raisepercent.global.exception.CompanyNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CompanyFacadeTest {
+
+    private static final CompanyRepository companyRepository = mock(CompanyRepository.class);
+    private static final CompanyFacade companyFacade = new CompanyFacade(companyRepository);
+
+    @Test
+    public void 회사_가져오기(){
+        String name = "google";
+
+        Company company = Company.builder()
+                .name(name)
+                .build();
+
+        when(companyRepository.findCompanyByName(name))
+                .thenReturn(Optional.of(company));
+
+        assertEquals(company,companyFacade.getCompany(name));
+    }
+
+    @Test
+    public void 회사_예외(){
+        String name = "google";
+
+        when(companyRepository.findCompanyByName(name))
+                .thenReturn(Optional.empty());
+
+        assertThrows(CompanyNotFoundException.class, () -> companyFacade.getCompany(name));
+    }
+
+}
