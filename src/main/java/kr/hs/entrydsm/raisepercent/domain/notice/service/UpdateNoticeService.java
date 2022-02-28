f5695a3e5bd0c234a3a802ad58c1eeac60d03553
cogofoward <hhgoon@naver.com>
2022-02-28 19:25:23 +0900

    📑 :: UpdateNoticeService

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/service/UpdateNoticeService.java
@@ -0,0 +1,26 @@
+package kr.hs.entrydsm.raisepercent.domain.notice.service;
+
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories.NoticeRepository;
+import kr.hs.entrydsm.raisepercent.domain.notice.facade.NoticeFacade;
+import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.request.UpdateNoticeRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateNoticeService {
+
+    private final NoticeFacade noticeFacade;
+    private final NoticeRepository noticeRepository;
+
+    public void execute(String noticeId, UpdateNoticeRequest updateNoticeRequest){
+
+        Notice notice = noticeFacade.getNotice(noticeId);
+
+        notice.updateTitle(updateNoticeRequest.getTitle());
+
+        notice.updateContent(updateNoticeRequest.getContent());
+    }
+
+}
