20f89c46bc4c31eef3c46c3f9c46ad9d9c866001
cogofoward <hhgoon@naver.com>
2022-02-28 19:24:20 +0900

    📑 :: NoticeFacade

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/facade/NoticeFacade.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.raisepercent.domain.notice.facade;
+
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories.NoticeRepository;
+import kr.hs.entrydsm.raisepercent.global.exception.NoticeNotFoundException;
+import kr.hs.entrydsm.raisepercent.global.util.UUIDUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class NoticeFacade {
+
+    private final NoticeRepository noticeRepository;
+
+    public Notice getNotice(String noticeId){
+        return noticeRepository.findById(UUIDUtil.convertToUUID(noticeId))
+                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
+    }
+
+}
