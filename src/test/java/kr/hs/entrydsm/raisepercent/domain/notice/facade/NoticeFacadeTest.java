c3720910d7addca0330a3d60707dcf16178e96e5
cogofoward <hhgoon@naver.com>
2022-02-28 19:24:36 +0900

    🧪 :: NoticeFacadeTest

---
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/notice/facade/NoticeFacadeTest.java
@@ -0,0 +1,52 @@
+package kr.hs.entrydsm.raisepercent.domain.notice.facade;
+
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.Notice;
+import kr.hs.entrydsm.raisepercent.domain.notice.domain.repositories.NoticeRepository;
+import kr.hs.entrydsm.raisepercent.global.exception.NoticeNotFoundException;
+import kr.hs.entrydsm.raisepercent.global.util.UUIDUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+
+
+
+public class NoticeFacadeTest {
+
+    private final NoticeRepository noticeRepository = mock(NoticeRepository.class);
+
+    private final NoticeFacade noticeFacade = new NoticeFacade(noticeRepository);
+
+    private final String id = "123e4567-e89b-12d3-a456-426614174000";
+
+    private final String noneExistId = "000e1234-a89c-32a3-a456-426614174000";
+
+    @Test
+    void 공지_가져오기(){
+        Notice notice = Notice.builder().build();
+
+        when(noticeRepository.findById(UUIDUtil.convertToUUID(id)))
+                .thenReturn(Optional.of(notice));
+
+        Notice findNotice = noticeFacade.getNotice(id);
+
+        assertEquals(notice,findNotice);
+    }
+
+    @Test
+    void 공지_존재_예외(){
+        when(noticeRepository.findById(UUIDUtil.convertToUUID(noneExistId)))
+                .thenReturn(Optional.empty());
+
+        assertThrows(NoticeNotFoundException.class, () -> noticeFacade.getNotice(noneExistId));
+    }
+
+}
