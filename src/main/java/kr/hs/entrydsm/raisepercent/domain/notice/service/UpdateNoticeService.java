200bf46f6e601bfa3623962645c83da2bb2a12f8
cogofoward <hhgoon@naver.com>
2022-02-28 22:58:54 +0900

    ♻️ :: @Transactional 추가

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/service/UpdateNoticeService.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/service/UpdateNoticeService.java
@@ -6,12 +6,15 @@ import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.request.Update
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @RequiredArgsConstructor
 @Service
 public class UpdateNoticeService {
 
     private final NoticeFacade noticeFacade;
 
+    @Transactional
     public void execute(String noticeId, UpdateNoticeRequest updateNoticeRequest) {
 
         Notice notice = noticeFacade.getNoticeById(noticeId);
