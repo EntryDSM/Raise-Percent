45a1fa499db90fe50d8cfa07329df424e44c3a4f
cogofoward <hhgoon@naver.com>
2022-02-28 20:04:15 +0900

    📝 :: NoticeController

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/presentation/NoticeController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/notice/presentation/NoticeController.java
@@ -1,20 +1,16 @@
 package kr.hs.entrydsm.raisepercent.domain.notice.presentation;
 
 import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.request.RegistrationNoticeRequest;
+import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.request.UpdateNoticeRequest;
 import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.response.NoticeDetailsResponse;
 import kr.hs.entrydsm.raisepercent.domain.notice.presentation.dto.response.NoticeListResponse;
 import kr.hs.entrydsm.raisepercent.domain.notice.service.NoticeListService;
 import kr.hs.entrydsm.raisepercent.domain.notice.service.RegistrationNoticeService;
 import kr.hs.entrydsm.raisepercent.domain.notice.service.ShowNoticeDetailsService;
+import kr.hs.entrydsm.raisepercent.domain.notice.service.UpdateNoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -26,6 +22,7 @@ public class NoticeController {
     private final RegistrationNoticeService registrationNoticeService;
     private final ShowNoticeDetailsService showNoticeDetailsService;
     private final NoticeListService noticeListService;
+    private final UpdateNoticeService updateNoticeService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -41,6 +38,12 @@ public class NoticeController {
     @GetMapping
     public NoticeListResponse noticeList() {
         return noticeListService.execute();
+    }
+
+    @PatchMapping("/{notice-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateNotice(@PathVariable("notice-id")String noticeId, @RequestBody UpdateNoticeRequest updateNoticeRequest){
+        updateNoticeService.execute(noticeId,updateNoticeRequest);
     }
 
 }
