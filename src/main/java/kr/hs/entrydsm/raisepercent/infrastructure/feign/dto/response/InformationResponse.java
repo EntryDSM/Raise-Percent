095018e8791f1f02379a82621a04205920ac7cec
Lee SeoJune <sung07288346@gmail.com>
2022-03-02 15:47:42 +0900

    📑 :: InformationResponse

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/infrastructure/feign/dto/response/InformationResponse.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.raisepercent.infrastructure.feign.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class InformationResponse {
+
+    private String name;
+    private String gcn;
+    private String email;
+
+}
