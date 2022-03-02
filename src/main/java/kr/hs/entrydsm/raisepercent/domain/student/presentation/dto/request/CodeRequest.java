4fbfb3cebd08c1168cf3c7fbd9b0144dd1278458
Lee SeoJune <sung07288346@gmail.com>
2022-03-02 15:47:57 +0900

    📑 :: CodeRequest dto

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/student/presentation/dto/request/CodeRequest.java
@@ -0,0 +1,12 @@
+package kr.hs.entrydsm.raisepercent.domain.student.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CodeRequest {
+
+    private String code;
+
+}
