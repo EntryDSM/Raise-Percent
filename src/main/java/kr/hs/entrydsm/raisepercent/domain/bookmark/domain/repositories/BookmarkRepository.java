d763e97a0728b26959dbf1da45b6127409b7b355
cogofoward <hhgoon@naver.com>
2022-03-24 18:37:24 +0900

    ⚡️ :: Add findByHrAndStudentEmail

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/bookmark/domain/repositories/BookmarkRepository.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/bookmark/domain/repositories/BookmarkRepository.java
@@ -1,14 +1,20 @@
 package kr.hs.entrydsm.raisepercent.domain.bookmark.domain.repositories;
 
 import kr.hs.entrydsm.raisepercent.domain.bookmark.domain.Bookmark;
+import kr.hs.entrydsm.raisepercent.domain.hr.domain.Hr;
 import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookmarkRepository extends CrudRepository<Bookmark, UUID> {
+
     List<Bookmark> findByStudent(Student student);
+
+    Optional<Bookmark> findByHrAndStudentEmail(Hr hr, String studentEmail);
+
 }
