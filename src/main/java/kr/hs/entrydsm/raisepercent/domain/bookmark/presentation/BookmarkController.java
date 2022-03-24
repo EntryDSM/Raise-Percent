9670ae2d98b5517f11b2a65eb3d61e4ea514fe57
cogofoward <hhgoon@naver.com>
2022-03-24 18:39:32 +0900

    📑 :: Create BookmarkController

---
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/bookmark/presentation/BookmarkController.java
@@ -0,0 +1,26 @@
+package kr.hs.entrydsm.raisepercent.domain.bookmark.presentation;
+
+import kr.hs.entrydsm.raisepercent.domain.bookmark.service.RegisterBookmarkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/bookmark")
+@RestController
+public class BookmarkController {
+
+    private final RegisterBookmarkService registerBookmarkService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{student-email}")
+    public void registerBookmark(@PathVariable("student-email") String studentEmail) {
+        registerBookmarkService.execute(studentEmail);
+    }
+
+}
