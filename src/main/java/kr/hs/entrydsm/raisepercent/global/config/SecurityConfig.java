9ac05e30833a8af213d63aaafe036169d979062b
cogofoward <hhgoon@naver.com>
2022-03-24 18:51:16 +0900

    ⚡️ :: Add endpoint

--- src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/global/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.DELETE, "/documents/public/{public-document-id}").hasRole("STUDENT")
                 .antMatchers(HttpMethod.POST, "/tags").hasRole("ROOT")
                 .antMatchers(HttpMethod.DELETE, "/tags/{tag-id}").hasRole("ROOT")
+                .antMatchers(HttpMethod.POST, "/bookmark/{student-email}").hasAnyRole("SENIOR","JUNIOR")
                 .anyRequest().authenticated()
                 .and().apply(new FilterConfig(jwtTokenProvider));
     }
