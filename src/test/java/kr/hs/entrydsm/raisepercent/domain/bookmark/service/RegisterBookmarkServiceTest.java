d3bce205831e821bab76c3f487041ade4bcd5e76
cogofoward <hhgoon@naver.com>
2022-04-03 22:04:40 +0900

    ♻️ :: AlreadyRegisteredBookmarkExcetpion test

--- src/test/java/kr/hs/entrydsm/raisepercent/domain/bookmark/service/RegisterBookmarkServiceTest.java
+++ src/test/java/kr/hs/entrydsm/raisepercent/domain/bookmark/service/RegisterBookmarkServiceTest.java
@@ -2,50 +2,90 @@ package kr.hs.entrydsm.raisepercent.domain.bookmark.service;
 
 import kr.hs.entrydsm.raisepercent.domain.bookmark.domain.Bookmark;
 import kr.hs.entrydsm.raisepercent.domain.bookmark.domain.repositories.BookmarkRepository;
+import kr.hs.entrydsm.raisepercent.domain.bookmark.exception.AlreadyRegisteredBookmarkException;
 import kr.hs.entrydsm.raisepercent.domain.hr.domain.Hr;
 import kr.hs.entrydsm.raisepercent.domain.hr.facade.HrFacade;
 import kr.hs.entrydsm.raisepercent.domain.student.domain.Student;
-import kr.hs.entrydsm.raisepercent.domain.student.domain.repositories.StudentRepository;
 import kr.hs.entrydsm.raisepercent.domain.student.facade.StudentFacade;
+
+import kr.hs.entrydsm.raisepercent.domain.user.domain.User;
 import org.junit.jupiter.api.Test;
-import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.mockito.BDDMockito.*;
+import static org.mockito.Mockito.mock;
 
 import java.util.Optional;
 
-import org.mockito.Mock;
-import org.mockito.InjectMocks;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 
 public class RegisterBookmarkServiceTest {
 
-    private static final RegisterBookmarkService registerBookmarkService = mock(RegisterBookmarkService.class);
-
     private static final BookmarkRepository bookmarkRepository = mock(BookmarkRepository.class);
+
+    private static final HrFacade hrFacade = mock(HrFacade.class);
 
     private static final StudentFacade studentFacade = mock(StudentFacade.class);
 
+    private static final RegisterBookmarkService service = new RegisterBookmarkService(
+            bookmarkRepository,
+            studentFacade,
+            hrFacade);
 
     @Test
     public void 북마크_등록하기() {
         String studentEmail = "example@dsm.hs.kr";
 
-        Hr hr = Hr.builder().build();
+        Hr hr = hrFacade.getHr();
         Bookmark bookmark = Bookmark.builder().build();
         Student student = studentFacade.getStudent(studentEmail);
 
-        when(bookmarkRepository.findByHrAndStudent(hr,student))
-                .thenReturn(Optional.empty());
+        //given
+        given(bookmarkRepository.findByHrAndStudent(hr, student))
+                .willReturn(Optional.empty());
 
-        registerBookmarkService.execute(studentEmail);
+        //when
+        service.execute(studentEmail);
 
-        when(bookmarkRepository.findByHrAndStudent(hr,student))
-                .thenReturn(Optional.of(bookmark));
+        //then
+        given(bookmarkRepository.findByHrAndStudent(hr, student))
+                .willReturn(Optional.of(bookmark));
+
+    }
+
+
+    @Test
+    public void 이미_존재하는_북마크() {
+        String studentEmail = "exampleStudent@dsm.hs.kr";
+
+        Hr hr = Hr.builder().build();
+
+        User user = User.builder()
+                .email(studentEmail)
+                .build();
+
+        Student student = Student.builder()
+                .user(user)
+                .build();
+
+        Bookmark bookmark = Bookmark.builder()
+                .student(student)
+                .hr(hr)
+                .build();
+
+        //given
+        given(hrFacade.getHr())
+                .willReturn(hr);
+
+        given(studentFacade.getStudent(studentEmail))
+                .willReturn(student);
+
+        given(bookmarkRepository.findByHrAndStudent(hr, student))
+                .willReturn(Optional.of(bookmark));
+
+        //then
+        assertThrows(AlreadyRegisteredBookmarkException.class, () -> service.execute(studentEmail));
+
     }
 
 }
