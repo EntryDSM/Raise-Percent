30f98226b4e06ddb11514cbbb1331438fda0f954
cogofoward <hhgoon@naver.com>
2022-04-07 20:32:00 +0900

    ♻️ :: divide controller

--- src/main/java/kr/hs/entrydsm/raisepercent/domain/hr/presentation/HrController.java
+++ src/main/java/kr/hs/entrydsm/raisepercent/domain/hr/presentation/HrController.java
@@ -1,20 +1,27 @@
 package kr.hs.entrydsm.raisepercent.domain.hr.presentation;
 
+import kr.hs.entrydsm.raisepercent.domain.hr.service.HrGoogleAuthService;
+import kr.hs.entrydsm.raisepercent.domain.user.presentation.dto.request.CodeRequest;
+import kr.hs.entrydsm.raisepercent.global.security.jwt.dto.TokenResponse;
 import kr.hs.entrydsm.raisepercent.global.util.auth.service.QueryGoogleAuthLinkService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/managers")
 @RestController
 public class HrController {
     private final QueryGoogleAuthLinkService queryGoogleAuthLinkService;
+    private final HrGoogleAuthService hrGoogleAuthService;
 
     @GetMapping("/auth")
     public String getLoginLink(){
         return queryGoogleAuthLinkService.execute();
     }
 
+    @PostMapping("/auth")
+    public ResponseEntity<TokenResponse> googleAuthLogin(@RequestBody CodeRequest codeRequest) {
+        return hrGoogleAuthService.execute(codeRequest);
+    }
 }
